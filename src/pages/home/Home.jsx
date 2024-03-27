eb8838aee606a97e8e35940f667755c686b00b1b
amitdutta <duttaamit356@gmail.com>
2024-03-27 07:55:46 +0530

    redux store setup

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,15 +6,30 @@ import myContext from '../../context/data/myContext'
 import ProductCard from '../../components/productCard/ProductCard'
 import Track from '../../components/track/Teack'
 import Testimonial from '../../components/testimonial/Testimonial'
+import { useDispatch, useSelector } from 'react-redux'
+import { addToCart, deleteFromCart } from '../../redux/cartSlice'
 
 
 const Home = () => {
 
 
+  const dispatch = useDispatch();
+  const cartItem = useSelector((state)=> state.cart)
 
+  console.log(cartItem)
+
+  const addCart = () => {
+    dispatch(addToCart("shirt"));
+  }
+
+  const deleteCart = () => {
+    dispatch(deleteFromCart("shirt"));
+  }
 
   return (
     <Layout>
+      <button className=' bg-gray-300 p-5' onClick={()=> addCart()}>add</button>
+        <button className=' bg-gray-300 p-5' onClick={()=> deleteCart()}>del</button>
       <HeroSection/>
       <Filter/>
       <ProductCard/>
