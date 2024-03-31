5a845be5f40c0c9f4adcda5fb478adafe062f9e6
amitdutta <duttaamit356@gmail.com>
2024-03-31 15:47:50 +0530

    add products to firebase

--- src/context/data/myState.jsx
+++ src/context/data/myState.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
 import MyContext from './myContext'
+import { fireDB } from '../../firebase/FirebaseConfig';
+import { Timestamp, addDoc, collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { toast } from 'react-toastify';
 
 const myState = (props) => {
 
-    
+
     const [mode, setMode] = useState('light');
 
     const toggleMode = () => {
@@ -20,8 +23,52 @@ const myState = (props) => {
 
     const [loading, setLoading] = useState(false);
 
+
+    const [products, setProducts] = useState({
+        title: null,
+        price: null,
+        imageUrl: null,
+        category: null,
+        description: null,
+        time: Timestamp.now(),
+        date: new Date().toLocaleString(
+            "en-US",
+            {
+                month: "short",
+                day: "2-digit",
+                year: "numeric",
+            }
+        )
+
+    })
+
+    // ********************** Add Product Section  **********************
+    const addProduct = async () => {
+        if (products.title == null || products.price == null || products.imageUrl == null || products.category == null || products.description == null) {
+            return toast.error('Please fill all fields')
+        }
+        const productRef = collection(fireDB, "products")
+        setLoading(true)
+        try {
+            await addDoc(productRef, products)
+            toast.success("Product Add successfully")
+            // getProductData()
+            // closeModal()
+            setLoading(false)
+        } catch (error) {
+            console.log(error)
+            setLoading(false)
+        }
+        setProducts("")
+    }
+
+    const [product, setProduct] = useState([]);
+
     return (
-        <MyContext.Provider value={{ mode, toggleMode, loading, setLoading }}>
+        <MyContext.Provider value={{
+            mode, toggleMode, loading, setLoading,
+            products, setProducts, addProduct
+        }}>
             {props.children}
         </MyContext.Provider>
     )
